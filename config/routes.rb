fcc34a8e3a9717f96cff8fd884ace91b26f99c78
Ngoc Truong <truong@tutanota.de>
2015-08-04 00:28:44 +0200

    add follow button and form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :sessions,    only: [:new, :create, :destroy]
-  resources :microposts,  only: [:create, :destroy]
+
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
+  #resources :users
+  resources :sessions,      only: [:new, :create, :destroy]
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   root 'static_pages#home'
   match '/help',  to: 'static_pages#help',  via: 'get'
